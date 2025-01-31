194936c3030bcde940630e2caa0ee0739b23666b
Loz Calver <loz@bigfork.co.uk>
2025-01-31 11:26:44 +0000

    Add option to disable Webp for envs compiled without support

--- src/WebpEnablerExtension.php
+++ src/WebpEnablerExtension.php
@@ -3,12 +3,21 @@
 namespace Bigfork\SilverstripeImageRecipe;
 
 use SilverStripe\Assets\Storage\AssetContainer;
+use SilverStripe\Core\Config\Configurable;
 use SilverStripe\Core\Extension;
 
 class WebpEnablerExtension extends Extension
 {
+    use Configurable;
+
+    private static bool $enabled = true;
+
     public function onBeforeRender()
     {
+        if (!$this->config()->get('enabled')) {
+            return;
+        }
+
         $candidates = $this->owner->getImageCandidates();
         foreach ($candidates as &$candidate) {
             $image = $candidate['image'] ?? null;
@@ -16,7 +25,7 @@ class WebpEnablerExtension extends Extension
                 continue;
             }
 
-            $candidate['image'] = $image->Convert('webp');
+            $candidate['image'] = $image->Webp();
         }
         $this->owner->setImageCandidates($candidates);
     }
